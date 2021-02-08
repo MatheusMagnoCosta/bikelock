943e1529176361e6b22bb32aedebff6ddf4657d8
Mathewsmag <madruder76@gmail.com>
2021-02-08 16:00:15 -0300

    first commit

---
+++ bikelock/src/main/java/br/app/bikelock/bikelock/BikelockApplication.java
@@ -0,0 +1,13 @@
+package br.app.bikelock.bikelock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikelockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikelockApplication.class, args);
+	}
+
+}
---
+++ bikelock/src/test/java/br/app/bikelock/bikelock/BikelockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.app.bikelock.bikelock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikelockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
